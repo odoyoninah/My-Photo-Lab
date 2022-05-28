867c668b0d89c1d52d45d62104061ce11fb11991
odoyonina <odoyoninah@gmail.com>
2022-05-28 22:24:03 +0300

    add url for categories and photos

--- photos/views.py
+++ photos/views.py
@@ -10,7 +10,8 @@ def index(request):
     return render(request, 'index.html', context)
 
 def viewPhoto(request, pk):
-    return render(request, 'photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photo.html', {'photo': photo})
 
 def addPhoto(request):
     return render(request, 'add.html')
