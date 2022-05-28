6043018177b327e1f6f42fb1e010bc21fa6294c0
odoyonina <odoyoninah@gmail.com>
2022-05-28 21:35:27 +0300

    register admin and superuser

--- Photolab/settings.py
+++ Photolab/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Category, Photo
+
+admin.site.register(Category)
+admin.site.register(Photo)
 
