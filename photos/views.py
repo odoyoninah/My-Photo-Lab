d4b65d908bc4769de78489d2205a31108266572c
odoyonina <odoyoninah@gmail.com>
2022-05-28 11:56:36 +0300

    create views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def image(request):
+    return render(request, 'photos/image.html')
+
+def viewPhoto(request):
+    return render(request, 'photos/photo.html')
+
+def addPhoto(request):
+    return render(request, 'photos/form.html')
