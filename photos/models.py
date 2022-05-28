d913e142ae372e2e8649c68c60af9357d830dafc
odoyonina <odoyoninah@gmail.com>
2022-05-28 19:23:02 +0300

    install pillow

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    description = models.TextField()
+    image = models.ImageField(null=False,blank=False)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
