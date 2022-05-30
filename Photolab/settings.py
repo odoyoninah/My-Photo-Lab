f2541de947a14105f8b010e4cd8dda4bc91a6b85
odoyonina <odoyoninah@gmail.com>
2022-05-30 18:04:09 +0300

    deployed to heroku

--- Photolab/settings.py
+++ Photolab/settings.py
@@ -160,8 +160,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
